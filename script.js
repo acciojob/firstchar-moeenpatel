fdaac4c1e0e58c7fdca62806803715411caf6552
moeenpatel <123477545+moeenpatel@users.noreply.github.com>
2023-09-21 22:10:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
 function firstChar(text) {
   // your 
-	let z="";
-	let n=text.length;
-	if(n==0 || text[0]=="" ||text[0]==" "){
-		return "";
-	}
-	for(int i=0;i<n;i++){
-		if(text[i]!=" "){
-			z=z+text[i];
-		}
-	}
-	return z[0];
+	text = text.trim();
+	if(text.length==0) return "";
+	return text.charAt(0) ;
+    // Return the first character of the trimmed string
+    return str[0];
 	
 }
 
