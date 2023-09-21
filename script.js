45eb2cca307678f53bf1ec315d849cbe7b548d07
moeenpatel <123477545+moeenpatel@users.noreply.github.com>
2023-09-21 22:07:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 function firstChar(text) {
-  // your code here
+  // your 
+	let z="";
+	let n=text.length;
+	if(n==0 || text[0]=="" ||text[0]==" "){
+		return "";
+	}
+	for(int i=0;i<n;i++){
+		if(text[i]!=" "){
+			z=z+text[i];
+		}
+	}
+	return z[0];
+	
 }
 
-// Do not change the code below
 
 const text = prompt("Enter text:");
 alert(firstChar(text));
